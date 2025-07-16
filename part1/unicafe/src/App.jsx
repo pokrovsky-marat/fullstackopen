efb7a972002c565b194c31de721b61856167ee41
Your Name <you@example.com>
2025-07-16 22:22:02 +0600

    1.7 ready

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,21 +1,51 @@
 import { useState } from "react";
+const Stat = ({ text, number }) => (
+  <div>
+    {text} {number}
+  </div>
+);
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+  const handleClick = (func, value) => () => {
+    let tempBad = bad;
+    let tempGood = good;
+    let tempAll = all;
+    if (func === setBad) {
+      tempBad++;
+    }
+    if (func === setGood) {
+      tempGood++;
+    }
+
+    func(value + 1);
+    tempAll++;
+    setAll(tempAll);
+
+    setAverage((tempGood - tempBad) / tempAll);
+    setPositive((tempGood / tempAll) * 100);
+  };
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={()=>{setGood(good+1)}}>good</button>
-      <button onClick={()=>{setNeutral(neutral+1)}}>neutral</button>
-      <button onClick={()=>{setBad(bad+1)}}>bad</button>
+      <button onClick={handleClick(setGood, good)}>good</button>
+      <button onClick={handleClick(setNeutral, neutral)}>neutral</button>
+      <button onClick={handleClick(setBad, bad)}>bad</button>
+
       <h2>statistics</h2>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
+      <Stat text={"good"} number={good} />
+      <Stat text={"neutral"} number={neutral} />
+      <Stat text={"bad"} number={bad} />
+      <Stat text={"all"} number={all} />
+      <Stat text={"average"} number={average} />
+      <Stat text={"positive"} number={positive} />
     </div>
   );
 };
