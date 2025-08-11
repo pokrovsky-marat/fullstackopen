78b9784d8b289b3df18feeae329ccf098dc795cd
Your Name <you@example.com>
2025-08-11 17:28:55 +0600

    3.8
    мы можем установить пакет morgan, это мидлваре для логирования
    в программе используем стандартным способом
    app.use(morgan('tiny')) с предустановленными настройками, плюс мы можем
    создавать свои кастомные токены(настройки)и использовать их в виде
    
    // Создаём токен для body
    morgan.token("body", (req) => JSON.stringify(req.body));
    
    app.use(
      morgan(":method :url :status :res[content-length] - :response-time ms
      :body")
      );
      Тут body это наш кастомный токен, всё остальное встроенное

--- part3/index.js
+++ part3/index.js
@@ -25,8 +25,12 @@ let persons = [
 ];
 
 app.use(express.json());
-app.use(morgan("tiny"));
+// Создаём токен для body
+morgan.token("body", (req) => JSON.stringify(req.body));
 
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
