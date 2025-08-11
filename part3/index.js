983935d1ecf67e6f6c3f01acf4b3ed64398f58ba
Your Name <you@example.com>
2025-08-11 17:09:06 +0600

    3.7
    Middlware в Nodejs это функция которую можно запускать перед обработкой
    запросов пример функции
    const requestLogger = (request, response, next) => {
    console.log('Method:', request.method)
    console.log('Path:  ', request.path)
    console.log('Body:  ', request.body)
    console.log('---')
    next()
     }
     Для использования нужно написать
     app.use(requestLogger)
     Если написать эту строчку после обработки запросов(get,post,delete)
     то функция отработает если никакой другой обработчик не перехватил
     запрос. Т.е можно использовать для обработки ошибки 404

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 let persons = [
   {
@@ -22,7 +23,10 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
 app.use(express.json());
+app.use(morgan("tiny"));
+
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
