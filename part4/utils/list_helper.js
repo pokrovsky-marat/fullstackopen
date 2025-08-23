052dfafe9a489fb43912094c5e64557a187cd39f
Your Name <you@example.com>
2025-08-24 01:06:16 +0600

    4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -5,14 +5,42 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => blogs.reduce((acc, item) => acc + item.likes, 0)
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null
-  result = blogs[0]
+  let result = blogs[0]
   blogs.forEach((blog) => {
     if (blog.likes > result.likes) result = blog
   })
   return result
 }
+//Нужно получить автора который встречается чаще всего
+//Данные представленны в виде массива объектов вида 👇
+// [
+//   {
+//     _id: '5a422bc61b54a676234d17fc',
+//     title: 'Type wars',
+//     author: 'Robert C. Martin',
+//     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+//     likes: 2,
+//     __v: 0,
+//   },
+// ]
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+  let authors = {}
+  blogs.forEach((blog) => {
+    if (!authors[blog.author]) authors[blog.author] = 1
+    else authors[blog.author]++
+  })
+  let result = Object.entries(authors).reduce((max, item) =>
+    item[1] > max[1] ? item : max
+  )
+  return {
+    author: result[0],
+    blogs: result[1],
+  }
+}
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
