44b3c09604766f061b72c0a59f0d4427aca48b51
Your Name <you@example.com>
2025-08-23 23:55:29 +0600

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,8 +3,16 @@ const dummy = (blogs) => {
   return 1
 }
 const totalLikes = (blogs) => blogs.reduce((acc, item) => acc + item.likes, 0)
-
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  result = blogs[0]
+  blogs.forEach((blog) => {
+    if (blog.likes > result.likes) result = blog
+  })
+  return result
+}
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
