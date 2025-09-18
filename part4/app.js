b715137ceb8511269c1d6f66f456a01aff8c8f01
Your Name <you@example.com>
2025-09-18 23:07:59 +0600

    4.20
    Вынес извлечение токена в мидлваре где прикрепил его к request

--- part4/app.js
+++ part4/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const { unknownEndpoint, errorHandler } = require('./utils/middlware')
+const {
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+} = require('./utils/middlware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -11,12 +15,12 @@ mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
     console.log(`Connected to ${config.MONGODB_URI}`)
-    
   })
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
 app.use(express.json())
+app.use(tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- part4/utils/middlware.js
+++ part4/utils/middlware.js
@@ -1,6 +1,13 @@
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknownEndpoint' })
 }
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+  next()
+}
 const errorHandler = (error, req, res, next) => {
   if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
@@ -9,9 +16,15 @@ const errorHandler = (error, req, res, next) => {
     error.message.includes('E11000 duplicate key error')
   ) {
     return res.status(400).json({ error: 'expected `username` to be unique' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'token invalid' })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({
+      error: 'token expired',
+    })
   }
 
   console.log(error)
   next(error)
 }
-module.exports = { unknownEndpoint, errorHandler }
+module.exports = { unknownEndpoint, errorHandler, tokenExtractor }
