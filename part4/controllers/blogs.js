13e9a62570efc125564bbd8a91282be5439674d4
Your Name <you@example.com>
2025-09-18 22:39:58 +0600

    4.19
    Сделал так что автор идентифицируется через выданный ему токен,
    из него получают айди user

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,20 +1,40 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const userId = request.body.user
-  const user = await User.findById(userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
-    return response.status(400).json({ error: 'userId missing or not valid' })
+    return response
+      .status(400)
+      .json({ error: 'user id is missing or not valid' })
   }
-  const blog = new Blog(request.body)
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+    user: user._id,
+  })
   const result = await blog.save()
   response.status(201).json(result)
   user.blogs = [...user.blogs, result._id]
