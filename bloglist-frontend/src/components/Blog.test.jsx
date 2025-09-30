3e2ee183c6f03f5715dd57ade48a7e8d6cd2aba7
Your Name <you@example.com>
2025-09-30 12:17:28 +0600

    5.14
    Установил библиотеку чтобы имитировать работу пользователя
    Написал тест для проверки что компонент оттображает доп данные при клике
    на кнопку view

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,23 +1,37 @@
 import { render, screen } from '@testing-library/react'
-
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-
-test('Хочу убедиться что Автор и название по умолчанию есть, а урла и лайков нет', async () => {
+describe('Проверяем работу компонента Blog', () => {
   const blog = {
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
+    username: 'savant',
   }
+  const user = { username: 'savant' }
 
-  render(<Blog blog={blog} />)
+  beforeEach(() => {
+    render(<Blog blog={blog} user={user} />)
+  })
+  test('Хочу убедиться что Автор и название по умолчанию есть, а урла и лайков нет', async () => {
+    const titleAndAuthor = screen.getByText('React patterns Michael Chan')
+    //Если я хочу убедиться что элементов нет использую queryByText
+    const url = screen.queryByText('https://reactpatterns.com/')
+    const likes = screen.queryByText('7')
 
-  const titleAndAuthor = screen.getByText('React patterns Michael Chan')
-  //Если я хочу убедиться что элементов нет использую queryByText
-  const url = screen.queryByText('https://reactpatterns.com/')
-  const likes = screen.queryByText('7')
+    expect(titleAndAuthor).toBeVisible()
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+  })
+  test("blog's URL and number of likes are shown when the button controlling the shown details has been clicked.", async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
 
-  expect(titleAndAuthor).toBeVisible()
-  expect(url).toBeNull()
-  expect(likes).toBeNull()
+    const url = screen.getByText('https://reactpatterns.com/')
+    const likes = screen.getByText('7')
+    expect(url).toBeVisible()
+    expect(likes).toBeVisible()
+  })
 })
