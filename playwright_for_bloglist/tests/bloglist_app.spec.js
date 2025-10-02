a2a1b62907d3c09304f0274ea434ab578bb84716
Your Name <you@example.com>
2025-10-02 20:14:35 +0600

    5.18
    Добавил в бэкенд раутер для тестрирования, он удаляет
    пользователей и записи из тестовой БД.
    Написал хелпер для тестов с повторяющимися действиями.
    Написал тест для проверки логина с правильными и неправильными
    данными.

--- playwright_for_bloglist/tests/bloglist_app.spec.js
+++ playwright_for_bloglist/tests/bloglist_app.spec.js
@@ -1,13 +1,33 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-
+const { loginWith } = require('./helper')
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Marat',
+        username: 'savant',
+        password: 'fP8aaasxh6Z_@',
+      },
+    })
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
     await expect(page.getByText('login')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      loginWith(page, 'savant', 'fP8aaasxh6Z_@')
+      await expect(page.getByText('Marat logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      loginWith(page, 'savant', 'wrong password')
+      await expect(page.getByText('Marat logged in')).not.toBeVisible()
+    })
   })
 })
