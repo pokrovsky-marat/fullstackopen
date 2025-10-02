ea8772c7adea6d1425f3e9457a46b60e77d5a570
Your Name <you@example.com>
2025-10-02 22:27:52 +0600

    5.22

--- playwright_for_bloglist/tests/bloglist_app.spec.js
+++ playwright_for_bloglist/tests/bloglist_app.spec.js
@@ -10,6 +10,13 @@ describe('Blog app', () => {
         password: 'fP8aaasxh6Z_@',
       },
     })
+    await request.post('/api/users', {
+      data: {
+        name: 'other',
+        username: 'other',
+        password: 'other',
+      },
+    })
     await page.goto('/')
   })
 
@@ -21,27 +28,27 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      loginWith(page, 'savant', 'fP8aaasxh6Z_@')
+      await loginWith(page, 'savant', 'fP8aaasxh6Z_@')
       await expect(page.getByText('Marat logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      loginWith(page, 'savant', 'wrong password')
+      await loginWith(page, 'savant', 'wrong password')
       await expect(page.getByText('Marat logged in')).not.toBeVisible()
     })
   })
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-      loginWith(page, 'savant', 'fP8aaasxh6Z_@')
+      await loginWith(page, 'savant', 'fP8aaasxh6Z_@')
     })
 
     test('a new blog can be created', async ({ page }) => {
-      createBlog(page, 'test title', 'test author', 'test url')
+      await createBlog(page, 'test title', 'test author', 'test url')
       await expect(page.getByText('test title test author')).toBeVisible()
     })
 
     test('a new blog can be liked', async ({ page }) => {
-      createBlog(page, 'test title', 'test author', 'test url')
+      await createBlog(page, 'test title', 'test author', 'test url')
       await page.getByRole('button', { name: 'view' }).click()
       await page.getByRole('button', { name: 'like' }).click()
       await page.getByText('1').waitFor({ timeout: 5000 })
@@ -49,11 +56,23 @@ describe('Blog app', () => {
     })
     test('a new blog can be deleted', async ({ page }) => {
       page.on('dialog', (dialog) => dialog.accept())
-      createBlog(page, 'test title', 'test author', 'test url')
+      await createBlog(page, 'test title', 'test author', 'test url')
       await page.getByRole('button', { name: 'view' }).click()
       await page.getByRole('button', { name: 'remove' }).click()
 
       await expect(page.getByText('test title test author')).not.toBeVisible()
+    })
+
+    test("only the user who added the blog sees the blog's delete button", async ({
+      page,
+    }) => {
+      await createBlog(page, 'test title', 'test author', 'test url')
+      await page.getByRole('button', { name: 'logout' }).click()
+
+      //другой пользователь логинится
+      await loginWith(page, 'other', 'other')
+      await page.getByRole('button', { name: 'view' }).click()
+      await expect(page.getByText('remove')).not.toBeVisible()
     })
   })
 })
