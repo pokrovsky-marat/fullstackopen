d9f9eef67da7e8619320f4a88e261f03f19ca2ef
Your Name <you@example.com>
2025-10-16 15:06:24 +0600

    6.13 complete
    Внедрил использования Нотификации при создании и голосовании

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,11 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { create_ac } from '../reducers/anecdoteReducer'
+import { notification_ac } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
   const create = (event) => {
     event.preventDefault()
     dispatch(create_ac(event.target.anecdote.value))
+    //Оттобразить сообщение на 5 секунд
+    dispatch(notification_ac(`You created '${event.target.anecdote.value}'`))
+    setTimeout(() => {
+      dispatch(notification_ac(''))
+    }, 5000)
     event.target.anecdote.value = ''
   }
   return (
--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { vote_ac } from '../reducers/anecdoteReducer'
+import { notification_ac } from '../reducers/notificationReducer'
 
 const Anecdote = ({ anecdote, vote }) => {
   return (
@@ -7,7 +8,9 @@ const Anecdote = ({ anecdote, vote }) => {
       <div>{anecdote.content}</div>
       <div>
         has {anecdote.votes}
-        <button onClick={() => vote(anecdote.id)}>vote</button>
+        <button onClick={() => vote(anecdote.id, anecdote.content)}>
+          vote
+        </button>
       </div>
     </div>
   )
@@ -19,9 +22,13 @@ const AnecdoteList = () => {
     return state.anecdotes.filter((item) => item.content.includes(filter))
   })
 
-  const vote = (id) => {
-    console.log('vote', id)
+  const vote = (id, content) => {
     dispatch(vote_ac(id))
+    //Оттобразить сообщение на 5 секунд
+    dispatch(notification_ac(`You voted '${content}'`))
+    setTimeout(() => {
+      dispatch(notification_ac(''))
+    }, 5000)
   }
 
   const sortedAnecdotes = anecdotes.toSorted((a, b) => b.votes - a.votes)
