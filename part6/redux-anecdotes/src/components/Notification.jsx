8de700bc6c2e2845a27713abb41452734f9c79a1
Your Name <you@example.com>
2025-10-16 14:47:36 +0600

    6.12 complete
    Создал редьюсер для работы с компонентом Notification, внес требуемые
    изменения в стор и добавил компонент в апп.жс

--- part6/redux-anecdotes/src/components/Notification.jsx
+++ part6/redux-anecdotes/src/components/Notification.jsx
@@ -1,12 +1,14 @@
+import { useSelector } from 'react-redux'
 const Notification = () => {
+  const notification = useSelector((state) => state.notification)
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    marginBottom: 10
+    marginBottom: 10,
   }
-
-  return <div style={style}>render here notification...</div>
+  if (notification) return <div style={style}>{notification}</div>
+  return null
 }
 
 export default Notification
