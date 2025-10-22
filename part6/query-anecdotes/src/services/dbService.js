f2aeea07e38c421d90834f03ec24f265a1334bc7
Your Name <you@example.com>
2025-10-22 07:05:38 +0600

    6.21 useMutation, useQueryClient
    Создаем новую запись посредством использования useMutation,
    useQueryClient

--- part6/query-anecdotes/src/services/dbService.js
+++ part6/query-anecdotes/src/services/dbService.js
@@ -1,7 +1,23 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
 export const getAll = async () => {
-  const response = await fetch('http://localhost:3001/anecdotes')
+  const response = await fetch(baseUrl)
   if (!response.ok) {
     throw new Error('Ошибка получения данных с сервера')
   }
   return response.json()
+}
+export const createAnecdote = async (anecdote) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(anecdote),
+  }
+
+  const response = await fetch(baseUrl, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to create note')
+  }
+
+  return await response.json()
 }
